bacb890a8327204be45397992d3914b701b92152
Gulg <65360617+GulgDev@users.noreply.github.com>
2023-02-13 11:14:44 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 class World2D {
     constructor(canvas) {
         this.canvasCtx = canvas.getContext("2d");
-        this.objects = [];
+        this.objects = {};
     }
     
-    createObject(x, y, image, mass) {
+    createObject(name, x, y, image, collider, mass) {
+        this.objects[name] = {
+            x: x,
+            y: y,
+            image: image,
+            collider: collider,
+            mass: mass
+        }
     }
 }
