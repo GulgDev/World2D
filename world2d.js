d0c4f8f4df6c7184d2699c0e9384d733c7cfb62a
Gulg <65360617+GulgDev@users.noreply.github.com>
2023-02-13 12:42:28 +0500

    Update world2d.js

--- world2d.js
+++ world2d.js
@@ -29,7 +29,7 @@ class World2D {
 class Sprite {
     constructor(position, image, collider, mass) {
         this.position = position;
-        this.velocity = [0, 0];
+        this.velocity = Vector2D.zero;
         this.image = Object.assign(document.createElement("img"), {
             src: image
         });
@@ -43,9 +43,21 @@ class Sprite {
     
     draw(canvasCtx) {
         if (this.imageLoaded) {
-            console.log(this.image.width, this.x);
-            canvasCtx.drawImage(this.image, this.x - this.image.width / 2, this.y - this.image.height / 2);
+            canvasCtx.drawImage(this.image, this.position.x - this.image.width / 2, this.position.y - this.image.height / 2);
         }
+    }
+}
+
+class Vector2 {
+    static zero = new Vector2(0, 0);
+    
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    add(other) {
+        return new Vector2(this.x + other.x, this.y + other.y);
     }
 }
 
