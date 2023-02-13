a159611ab0b37273b6631daab636f300923de652
Gulg <65360617+GulgDev@users.noreply.github.com>
2023-02-13 11:05:52 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+class World2D {
+    constructor(canvas) {
+        this.canvasCtx = canvas.getContext("2d");
+        this.objects = [];
+    }
+    
+    createObject(x, y, image, mass) {
+    }
+}
